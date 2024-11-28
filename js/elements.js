2ffc3ce21773bfa2ca8815436f5ccfe23d56030a
ludwig <ludwigstridh@gmail.com>
2024-11-28 15:27:58 +0100

    done with functionality

---
+++ js/elements.js
@@ -0,0 +1,28 @@
+export const planets = {
+	solen: document.querySelector(".sun"),
+	merkurius: document.querySelector(".mercury"),
+	venus: document.querySelector(".venus"),
+	jorden: document.querySelector(".earth"),
+	mars: document.querySelector(".mars"),
+	jupiter: document.querySelector(".jupiter"),
+	saturnus: document.querySelector(".saturn"),
+	uranus: document.querySelector(".uranus"),
+	neptunus: document.querySelector(".neptune"),
+};
+
+export const displayData = {
+	planetName: document.querySelector(".planet__name"),
+	latin: document.querySelector(".latin"),
+	infoText: document.querySelector(".info__text"),
+	omkretsInfo: document.querySelector(".omkrets__info"),
+	distansKm: document.querySelector(".distans__km"),
+	maxTemp: document.querySelector(".max-temp__c"),
+	minTemp: document.querySelector(".min-temp__c"),
+	moon: document.querySelector(".moons_moon"),
+};
+
+export const changePage = {
+	planets: document.querySelector(".planet-page"),
+	info: document.querySelector(".data"),
+	button: document.querySelector(".back"),
+};
---
+++ js/fetchData.js
@@ -0,0 +1,27 @@
+
+//Function to fetch data for key and then use it to fetch data for planets
+
+export const getData = async () => {
+	try {
+		const keyResponse = await fetch(
+			"https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys",
+			{
+				method: "POST",
+			}
+		);
+		const keyData = await keyResponse.json();
+
+		const dataResponse = await fetch(
+			"https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+			{
+				method: "GET",
+				headers: { "x-zocom": keyData.key },
+			}
+		);
+		const finalData = await dataResponse.json();
+
+		return finalData;
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+};
